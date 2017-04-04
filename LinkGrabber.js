51b682811c99a22a3ab07ae479821d8c7f9b1577
BrandonWyatt <wyabr520@student.otago.ac.nz>
2017-04-05 00:04:10 +1200

    A few changes to the LinkGrabber
    Parsing JSON better now, about to work on user input and then returning meaningful output

--- LinkGrabber.js
+++ LinkGrabber.js
@@ -1,13 +1,19 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var request = new XMLHttpRequest();
 var siteStr = "https://api.cognitive.microsoft.com/bing/v5.0/search?q=";
-var query = "hello";
+var query = "Andrew Trotmans papers";
 var key = "f696d60cdc8a4cc68a8f39f33bb150be";
 request.open("GET", siteStr+query, false);
-request.setRequestHeader("Ocp-Apim-Subscription-Key", key);
+request.setRequestHeader("Ocp-Apim-Subscription-Key", key, "BingAPIs-Market", "en-NZ");
+
+request.responseType = 'document';
 
 request.send();
 
-console.log(request.status);
-console.log(request.statusText);
-console.log(request.responseText);
+var test = JSON.parse(request.responseText)
+
+console.log(test.webPages)
+
+//console.log(request.status);
+//console.log(request.statusText);
+//console.log(request.responseText);
