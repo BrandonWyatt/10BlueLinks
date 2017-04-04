5aae1b7198563f24e43cf74f84f7f4ee89ebc430
BrandonWyatt <wyabr520@student.otago.ac.nz>
2017-04-05 01:59:18 +1200

    Functional update
    This program now prints out the information for the preset number of pages, which may be increased manually from 10. The user must enter a search term on the command line as an addition argument, otherwise no search term will be entered and the program will take measures to avoid crashing.

--- LinkGrabber.js
+++ LinkGrabber.js
@@ -1,10 +1,10 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var request = new XMLHttpRequest();
-var readline = require('readline');
 
 var siteStr = "https://api.cognitive.microsoft.com/bing/v5.0/search?q=";
-var query = process.argv.slice(2);
 var key = "f696d60cdc8a4cc68a8f39f33bb150be";
+var count = 10
+var query = process.argv.slice(2);
 
 
 request.open("GET", siteStr+query, false);
@@ -14,10 +14,18 @@ request.responseType = 'document';
 
 request.send();
 
-var test = JSON.parse(request.responseText)
+var jsontext = JSON.parse(request.responseText)
 
-console.log(test.webPages)
+console.log(""); //print an extra line for "prettiness"
 
-//console.log(request.status);
-//console.log(request.statusText);
-//console.log(request.responseText);
+if (Math.floor(request.status/100)==2){
+  for (var c = 0; c<count; c++){
+    console.log(jsontext.webPages.value[c].name)
+    console.log(jsontext.webPages.value[c].displayUrl)
+    console.log(jsontext.webPages.value[c].snippet)
+    console.log(jsontext.webPages.value[c].url+"\n")
+  }
+}
+if (Math.floor(request.status/100)!=2){
+  console.log("Unable to complete request. Did you specify a search query in the command line?")
+}
