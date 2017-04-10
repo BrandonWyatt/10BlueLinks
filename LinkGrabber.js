05149b263d92907a27f05b5607222f6f88e8a545
BrandonWyatt <wyabr520@student.otago.ac.nz>
2017-04-11 03:12:11 +1200

    Work so far

--- LinkGrabber.js
+++ LinkGrabber.js
@@ -1,13 +1,18 @@
+// TO-DO
+// - Figure out why there are discrepancies between website and API (even though API results seem better)
+// - Write up work done so far in report
+// - What next?
+
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var request = new XMLHttpRequest();
+var resultRequest = new XMLHttpRequest();
 
 var siteStr = "https://api.cognitive.microsoft.com/bing/v5.0/search?q=";
 var key = "f696d60cdc8a4cc68a8f39f33bb150be";
-var count = 10
-var query = process.argv.slice(2);
+var count = 2
+var query = process.argv.slice(2).join(" ");
 
-
-request.open("GET", siteStr+query, false);
+request.open("GET", siteStr+encodeURIComponent(query)+"&count="+count, false);
 request.setRequestHeader("Ocp-Apim-Subscription-Key", key, "BingAPIs-Market", "en-NZ");
 
 request.responseType = 'document';
@@ -18,13 +23,21 @@ var jsontext = JSON.parse(request.responseText)
 
 console.log(""); //print an extra line for "prettiness"
 
-if (Math.floor(request.status/100)==2){
-  for (var c = 0; c<count; c++){
-    console.log(jsontext.webPages.value[c].name)
-    console.log(jsontext.webPages.value[c].displayUrl)
-    console.log(jsontext.webPages.value[c].snippet)
-    console.log(jsontext.webPages.value[c].url+"\n")
-  }
+if (Math.floor(request.status/100)==2){ // If status code indicates success
+
+    for(values of jsontext.webPages.value){
+      console.log(values.name)
+      //console.log(values.displayUrl)
+      //console.log(values.snippet)
+      //console.log(values.url)
+      console.log()
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.open("GET", values.url, false);
+      xmlhttp.send();
+      console.log(xmlhttp.status)
+      console.log(xmlhttp.responseType) // why undefined
+      console.log(xmlhttp.responseText) // why blank line
+    }
 }
 if (Math.floor(request.status/100)!=2){
   console.log("Unable to complete request. Did you specify a search query in the command line?")
