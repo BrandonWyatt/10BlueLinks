5e21416d8b5a4d9719ab232bff16c9d9741dfb8b
BrandonWyatt <wyabr520@student.otago.ac.nz>
2017-04-17 01:33:13 +1200

    Beginning of report
    LinkGrabber has the ability to strip out the usable URL and use this to access the HTML for the links returned. Next step is to parse the DOM elements and determine the frequency of the terms in the search.

--- LinkGrabber.js
+++ LinkGrabber.js
@@ -9,7 +9,7 @@ var resultRequest = new XMLHttpRequest();
 
 var siteStr = "https://api.cognitive.microsoft.com/bing/v5.0/search?q=";
 var key = "f696d60cdc8a4cc68a8f39f33bb150be";
-var count = 2
+var count = 5
 var query = process.argv.slice(2).join(" ");
 
 request.open("GET", siteStr+encodeURIComponent(query)+"&count="+count, false);
@@ -21,23 +21,35 @@ request.send();
 
 var jsontext = JSON.parse(request.responseText)
 
-console.log(""); //print an extra line for "prettiness"
-
 if (Math.floor(request.status/100)==2){ // If status code indicates success
-
+var reqCount = 0
     for(values of jsontext.webPages.value){
       console.log(values.name)
-      //console.log(values.displayUrl)
-      //console.log(values.snippet)
-      //console.log(values.url)
-      console.log()
+      console.log(values.displayUrl)
+      console.log(values.snippet)
+      console.log(values.url)
+      var newurl = values.url.substring(20, values.url.length)
+      newurl = newurl.substring(newurl.search("http"), newurl.length)
+      newurl = newurl.substring(0, newurl.search("&p=DevEx"))
+      newurl = decodeURIComponent(newurl)
+      console.log(newurl)
       var xmlhttp = new XMLHttpRequest();
-      xmlhttp.open("GET", values.url, false);
+      xmlhttp.open("GET", newurl, false);
+      xmlhttp.responseType = 'document'
       xmlhttp.send();
+      if (xmlhttp.status === 301){
+        newurlsecure = newurl.slice(0, 4) + "s" + newurl.slice(4)
+        xmlhttp.open("GET", newurlsecure, false);
+        xmlhttp.responseType = 'document'
+        xmlhttp.send();
+      }
       console.log(xmlhttp.status)
-      console.log(xmlhttp.responseType) // why undefined
-      console.log(xmlhttp.responseText) // why blank line
+      //console.log(xmlhttp.responseText)
+      reqCount++
+      console.log(""); //print an extra line for "prettiness"
+
     }
+    //console.log(reqCount)
 }
 if (Math.floor(request.status/100)!=2){
   console.log("Unable to complete request. Did you specify a search query in the command line?")
